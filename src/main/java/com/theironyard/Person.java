5cebb746462c5cefd6fc8b72004a9aed19a0b142
Doug Hughes <doug@theironyard.com>
2017-04-14 08:48:17 -0400

    yesterday's work

---
+++ src/main/java/com/theironyard/Person.java
@@ -0,0 +1,71 @@
+package com.theironyard;
+
+/**
+ * Created by doug on 4/13/17.
+ */
+public class Person {
+
+    private Integer personId;
+    private String title;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String suffix;
+
+    public Person(Integer personId, String title, String firstName, String middleName, String lastName, String suffix) {
+        this.personId = personId;
+        this.title = title;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.suffix = suffix;
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+}
